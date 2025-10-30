1da8dafcde3adb6736713f49fd7bd2f316ffedda
Seonggyun01 <thdrbs010314@naver.com>
2025-10-30 12:11:36 +0900

    feat(domain): Yield 클래스 추가
    - calculate():  수익률 계산 기능
      - 당첨금/ 구입금액 * 100
      - 소수점 둘째자리에서 반올림

--- src/main/java/lotto/domain/Yield.java
+++ src/main/java/lotto/domain/Yield.java
@@ -1,4 +1,25 @@
 package lotto.domain;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Yield {
+    private Yield() {
+    }
+
+    /**
+     * 수익률 계산(소수점 2자리에서 올림)
+     *
+     * @param winningPrize Long
+     * @param money        int
+     * @return 수입률 BigDecimal
+     */
+    public static BigDecimal calculate(Long winningPrize, int money) {
+        BigDecimal winningPrizeDecimal = BigDecimal.valueOf(winningPrize);
+        BigDecimal moneyDecimal = BigDecimal.valueOf(money);
+
+        return winningPrizeDecimal
+                .multiply(new BigDecimal("100"))
+                .divide(moneyDecimal, 1, RoundingMode.HALF_UP);
+    }
 }
