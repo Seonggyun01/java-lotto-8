c25038db9c8f5c7874e6e4adc4a0d59d7da50592
Seonggyun01 <thdrbs010314@naver.com>
2025-10-30 12:08:41 +0900

    feat(domain): Money 클래스 추가
    - 구입 금액을 가지고 있는 클래스
    - buyLotto(): 구입 금액으로 로또를 구매하는 기능

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,4 +1,13 @@
 package lotto.domain;
 
 public class Money {
+    private final int money;
+
+    public Money(int money) {
+        this.money = money;
+    }
+
+    public int buyLotto() {
+        return money / 1000;
+    }
 }
