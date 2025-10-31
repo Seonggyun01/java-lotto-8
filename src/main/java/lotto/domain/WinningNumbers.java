c4c0190f48b24623895133fa80ac340a6fcb5498
Seonggyun01 <thdrbs010314@naver.com>
2025-10-31 13:11:47 +0900

    feat(domain): WinningNumbers 클래스 추가
    - 당첨 번호 검증
    - 각 숫자를 NumberPolicy로 검증

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,4 +1,29 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class WinningNumbers {
+    private static final int WINNING_NUMBER_SIZE = 6;
+    private final List<Integer> numbers;
+
+    public WinningNumbers(List<Integer> numbers) {
+        validate(numbers);
+        validateNumber(numbers);
+        this.numbers = numbers;
+    }
+    private static void validateNumber(List<Integer> numbers) {
+        for (int num : numbers) {
+            NumberPolicy.validate(num);
+        }
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != WINNING_NUMBER_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
