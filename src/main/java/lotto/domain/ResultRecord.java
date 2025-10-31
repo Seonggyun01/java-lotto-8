f2e9640f04ed1da4821ea7c06540b4fae30eac16
Seonggyun01 <thdrbs010314@naver.com>
2025-10-31 13:13:15 +0900

    feat(domain): ResultRecord 클래스 추가
    - 로또 등수를 기록하는 클래스
    - Rank를 키로 사용하기 때문에 EnumMap을 사용하기로 결정
    - record(): Rank를 입력 받으면 해당 값이 1씩 증가

--- src/main/java/lotto/domain/ResultRecord.java
+++ src/main/java/lotto/domain/ResultRecord.java
@@ -1,4 +1,25 @@
 package lotto.domain;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 public class ResultRecord {
+    private static final int INITIAL_COUNT = 0;
+    private final Map<Rank, Integer> resultRecord = new EnumMap<>(Rank.class);
+
+    public ResultRecord() {
+        for (Rank rank : Rank.values()) {
+            resultRecord.put(rank, INITIAL_COUNT);
+        }
+    }
+
+    public void record(Rank rank) {
+        Integer value = resultRecord.get(rank);
+        value++;
+        resultRecord.put(rank, value);
+    }
+
+    public Map<Rank, Integer> getMap(){
+        return resultRecord;
+    }
 }
