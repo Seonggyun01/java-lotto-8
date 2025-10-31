336264e1b06f51cc94370b0c9acafc6fe01c6243
Seonggyun01 <thdrbs010314@naver.com>
2025-10-31 12:58:39 +0900

    feat(domain): NumberPolicy 클래스 추가
    - 각 숫자 검증
      - 숫자에 0이 들어있는 경우
      - 숫자가 범위를 벗어난 경우

--- src/main/java/lotto/domain/NumberPolicy.java
+++ src/main/java/lotto/domain/NumberPolicy.java
@@ -1,4 +1,22 @@
 package lotto.domain;
 
-public class NumberPolisy {
+import lotto.domain.exception.NumErrorMessage;
+
+public class NumberPolicy {
+    private static final int MIN_NUMBER_RANGE = 1;
+    private static final int MAX_NUMBER_RANGE = 45;
+
+    private NumberPolicy() {
+    }
+
+    public static void validate(int number) {
+        //숫자에 0이 들어있는 경우 “번호에 0을 입력할 수 없습니다.”
+        if (number == 0) {
+            throw new IllegalStateException(NumErrorMessage.ZERO_NOT_ALLOWED.getMessage());
+        }
+        // 숫자가 범위를 벗어난 경우 “로또 번호는 1부터 45 사이의 숫자여야 합니다.”
+        if (number < MIN_NUMBER_RANGE || number > MAX_NUMBER_RANGE) {
+            throw new IllegalStateException(NumErrorMessage.OUT_OF_RANGE.getMessage());
+        }
+    }
 }
