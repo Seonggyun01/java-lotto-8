ce4b4488f960f25b7523bc27ccc69c0d4292028a
Seonggyun01 <thdrbs010314@naver.com>
2025-10-31 13:16:49 +0900

    feat(util): Parser 클래스 추가
    - 쉼표를 기준으로 분리
    - 나눈 후 양쪽 공백 제거
    - 문자열 리스트로 반환

--- src/main/java/lotto/util/Parser.java
+++ src/main/java/lotto/util/Parser.java
@@ -1,4 +1,25 @@
 package lotto.util;
 
-public class Parsor {
+import static java.util.List.copyOf;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Parser {
+    private Parser() {
+    }
+
+    /**
+     * 당첨 번호 쉽표 기준으로 나누기
+     *
+     * @param rawWinningNumbers String
+     * @return List<Integer>
+     */
+    public static List<String> parse(String rawWinningNumbers) {
+        List<String> parsedWinningNumbers = Stream.of(rawWinningNumbers.split(",", -1))
+                .map(String::trim)
+                .toList();
+
+        return copyOf(parsedWinningNumbers);
+    }
 }
