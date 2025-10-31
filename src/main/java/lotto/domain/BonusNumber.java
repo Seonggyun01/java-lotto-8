85d259d08ea42ad94ee45105af12325b6c0d6135
Seonggyun01 <thdrbs010314@naver.com>
2025-10-31 13:10:40 +0900

    feat(domain): BonusNumber 클래스 추가
    - NumberPolicy로 검증 책임

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,4 +1,14 @@
 package lotto.domain;
 
 public class BonusNumber {
+    private final int number;
+
+    public BonusNumber(int number) {
+        NumberPolicy.validate(number);
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
