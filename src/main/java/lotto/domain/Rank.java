95867030cff92f4c055d92144c2d1d2599e69c20
Seonggyun01 <thdrbs010314@naver.com>
2025-10-31 13:04:03 +0900

    feat(domain): Rank enum 추가
    - 맞은 개수에 따라 1~5등 정의
    - check(): 맞은 개수와 보너스 번호 유무에 따라 맞는 Rank를 반환

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,4 +1,46 @@
 package lotto.domain;
 
 public enum Rank {
+    FIRST(6, 2_000_000_000L),
+    SECOND(5, 30_000_000L),
+    THIRD(5, 1_500_000L),
+    FOURTH(4, 50_000L),
+    FIFTH(3, 5_000L),
+    FAIL(0, 0L);
+
+    private int matchCount;
+    private Long prize;
+
+    Rank(int i, Long i1) {
+    }
+
+    /**
+     * 맞은 숫자 개수와 보너스 번호 유무에 따라서 Rank 반환
+     *
+     * @param matchCount
+     * @param matchBonusNum
+     * @return
+     */
+    public static Rank checkRank(int matchCount, boolean matchBonusNum) {
+        if (matchCount == 6) {
+            return FIRST;
+        }
+        if (matchCount == 5 && matchBonusNum) {
+            return SECOND;
+        }
+        if (matchCount == 5) {
+            return THIRD;
+        }
+        if (matchCount == 4) {
+            return FOURTH;
+        }
+        if (matchCount == 3) {
+            return FIFTH;
+        }
+        return FAIL;
+    }
+
+    public Long getPrize() {
+        return prize;
+    }
 }
