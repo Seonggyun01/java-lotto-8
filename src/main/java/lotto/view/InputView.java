549584041d2b0ae66b182ff0a621aed72e2d0e23
Seonggyun01 <thdrbs010314@naver.com>
2025-11-02 16:23:18 +0900

    feat(view): InputView 클래스 추가
    - 사용자 입력을 받는 기능
    - readMoney(): 구입 금액
    - readWinningNumbers(): 당첨 번호
    - readBonusNumber(): 보너스 번호

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,18 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public static String readMoney(){
+        return Console.readLine();
+    }
+
+    public static String readWinningNumbers(){
+        return Console.readLine();
+    }
+
+    public static String readBonusNumber() {
+        return Console.readLine();
+    }
 }
