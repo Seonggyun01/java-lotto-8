22b1f1efcf492faf63156d26d40158f82e8bb0f0
Seonggyun01 <thdrbs010314@naver.com>
2025-11-02 17:31:54 +0900

    feat(exception): MoneyErrorMessage enum 추가

--- src/main/java/lotto/domain/exception/MoneyErrorMessage.java
+++ src/main/java/lotto/domain/exception/MoneyErrorMessage.java
@@ -1,13 +1,13 @@
 package lotto.domain.exception;
 
 public enum MoneyErrorMessage {
-    EMPTY_INPUT("구입 금액을 입력해주세요."),
-    INVALID_DECIMAL("구입 금액은 1,000원 이상 자연수를 입력해 주세요."),
-    CONTAINS_CHARACTER("구입 금액에 문자는 입력할 수 없습니다."),
-    TOO_LARGE("너무 큰 금액입니다."),
-    NEGATIVE_INPUT("구입 금액에 음수는 입력할 수 없습니다."),
-    BELOW_MINIMUM("1,000원 이상 금액을 입력해주세요."),
-    INVALID_UNIT("구입 금액은 1,000원 단위입니다.");
+    EMPTY_INPUT("[ERROR] 구입 금액을 입력해주세요."),
+    INVALID_DECIMAL("[ERROR] 구입 금액은 1,000원 이상 자연수를 입력해 주세요."),
+    CONTAINS_CHARACTER("[ERROR] 구입 금액에 문자는 입력할 수 없습니다."),
+    TOO_LARGE("[ERROR] 너무 큰 금액입니다."),
+    NEGATIVE_INPUT("[ERROR] 구입 금액에 음수는 입력할 수 없습니다."),
+    BELOW_MINIMUM("[ERROR] 1,000원 이상 금액을 입력해주세요."),
+    INVALID_UNIT("[ERROR] 구입 금액은 1,000원 단위입니다.");
 
     private String message;
 
