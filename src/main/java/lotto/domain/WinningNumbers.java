2610a43a79b4a5b559da977446ea494a3d61570e
Seonggyun01 <thdrbs010314@naver.com>
2025-11-02 17:32:44 +0900

    feat(domain): 중복 검사, 오름 차순 정렬 기능 추가
    - sortNumbers(): 오름 차순 정렬
    - validateDuplicate(): 중복 숫자 검사

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,16 +1,34 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import lotto.domain.exception.NumErrorMessage;
 
 public class WinningNumbers {
     private static final int WINNING_NUMBER_SIZE = 6;
     private final List<Integer> numbers;
 
     public WinningNumbers(List<Integer> numbers) {
-        validate(numbers);
-        validateNumber(numbers);
-        this.numbers = numbers;
+        List<Integer> copy = new ArrayList<>(numbers);
+        validate(copy);
+        validateNumber(copy);
+        validateDuplicate(numbers);
+        sortNumbers(copy);
+        this.numbers = copy;
     }
+
+    private static void sortNumbers(List<Integer> numbers) {
+        Collections.sort(numbers);
+    }
+
+    private static void validateDuplicate(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() != WINNING_NUMBER_SIZE) {
+            throw new IllegalArgumentException(NumErrorMessage.DUPLICATE_NUMBER.getMessage());
+        }
+    }
+
     private static void validateNumber(List<Integer> numbers) {
         for (int num : numbers) {
             NumberPolicy.validate(num);
@@ -19,7 +37,7 @@ public class WinningNumbers {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != WINNING_NUMBER_SIZE) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(NumErrorMessage.WINNING_NUMBER_SIZE.getMessage());
         }
     }
 
