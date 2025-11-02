ed2d18971d7fbc23cd0b1ff214f640b72663cdc3
Seonggyun01 <thdrbs010314@naver.com>
2025-11-02 17:30:25 +0900

    feat(exception): NumErrormessage enum 추가

--- src/main/java/lotto/domain/exception/NumErrorMessage.java
+++ src/main/java/lotto/domain/exception/NumErrorMessage.java
@@ -1,11 +1,14 @@
 package lotto.domain.exception;
 
 public enum NumErrorMessage {
-    EMPTY_INPUT("번호를 입력해주세요."),
-    INVALID_DECIMAL("정수를 입력해주세요."),
-    CONTAINS_CHARACTER("번호에 문자는 입력할 수 없습니다."),
-    ZERO_NOT_ALLOWED("번호에 0을 입력할 수 없습니다."),
-    OUT_OF_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    EMPTY_INPUT("[ERROR] 번호를 입력해주세요."),
+    INVALID_DECIMAL("[ERROR] 정수를 입력해주세요."),
+    CONTAINS_CHARACTER("[ERROR] 번호에 문자는 입력할 수 없습니다."),
+    ZERO_NOT_ALLOWED("[ERROR] 번호에 0을 입력할 수 없습니다."),
+    OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATE_NUMBER("[ERROR] 로또 번호는 중복될 수 없습니다."),
+    LOTTO_NUMBER_SIZE("[ERROR] 로또 번호는 6개여야 합니다."),
+    WINNING_NUMBER_SIZE("[ERROR] 당첨 번호는 6개여야 합니다.");
 
     private final String message;
 
