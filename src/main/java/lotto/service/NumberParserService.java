26f13ad33e01f3c5b7eb86bd86a38b63669a0247
Seonggyun01 <thdrbs010314@naver.com>
2025-11-03 12:08:40 +0900

    feat(service): NumberParserService 클래스 추가
    - 입력 받은 당첨번호 문자열을 분리하고 검증하는 서비스
    - 입력 받은 보너스 번호 문자열을 검증하는 서비스

--- src/main/java/lotto/service/NumberParserService.java
+++ src/main/java/lotto/service/NumberParserService.java
@@ -1,4 +1,27 @@
 package lotto.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.util.Parser;
+import lotto.validator.NumValidate;
+
+/**
+ * 컨트롤러에서 받은 당첨 번호 분리 및 검증
+ */
 public class NumberParserService {
+    public List<Integer> parseNumbers(String rawNumbers){
+        List<String> tokens = Parser.parse(rawNumbers);
+        List<Integer> result = new ArrayList<>();
+
+        //당첨 금액 검증 및 생성
+        for(String token : tokens){
+            int validateNum = NumValidate.validate(token);
+            result.add(validateNum);
+        }
+        return result;
+    }
+
+    public int parseSingleNumber(String rawNumber){
+        return NumValidate.validate(rawNumber);
+    }
 }
