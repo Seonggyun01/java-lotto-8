092f75af2ccd1b67ad4157c9840d35de71517331
Seonggyun01 <thdrbs010314@naver.com>
2025-11-03 12:55:07 +0900

    test: Rank 도메인 단위 테스트 추가
    - 1등 조건 테스트
    - 2등 조건 테스트
    - 3등 조건 테스트
    - 4등 조건 테스트
    - 5등 조건 테스트
    - FAIL 조건 테스트

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -1,4 +1,47 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 class RankTest {
-  
+
+    @DisplayName("1등 테스트")
+    @Test
+    void first_rank() {
+        assertThat(Rank.checkRank(6, false)).isEqualTo(Rank.FIRST);
+    }
+
+    @DisplayName("2등 테스트")
+    @Test
+    void second_rank() {
+        assertThat(Rank.checkRank(5, true)).isEqualTo(Rank.SECOND);
+    }
+
+    @DisplayName("3등 테스트")
+    @Test
+    void third_rank() {
+        assertThat(Rank.checkRank(5, false)).isEqualTo(Rank.THIRD);
+    }
+
+    @DisplayName("4등 테스트")
+    @Test
+    void fourth_rank() {
+        assertThat(Rank.checkRank(4, false)).isEqualTo(Rank.FOURTH);
+    }
+
+    @DisplayName("5등 테스트")
+    @Test
+    void fifth_rank() {
+        assertThat(Rank.checkRank(3, false)).isEqualTo(Rank.FIFTH);
+    }
+
+    @DisplayName("FAIL 테스트")
+    @Test
+    void fail_rank() {
+        assertThat(Rank.checkRank(2, false)).isEqualTo(Rank.FAIL);
+        assertThat(Rank.checkRank(1, false)).isEqualTo(Rank.FAIL);
+        assertThat(Rank.checkRank(0, false)).isEqualTo(Rank.FAIL);
+    }
 }
