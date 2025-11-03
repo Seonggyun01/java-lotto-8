269ba3742e29251b9dffd180ec4fb6fe14312252
Seonggyun01 <thdrbs010314@naver.com>
2025-11-03 12:10:18 +0900

    feat(service): WinningNumberService 클래스 추가
    - 파싱된 숫자리스트로 당첨번호 객체 생성
    - 보너스 번호가 당첨번호와 중복인지 검사

--- src/main/java/lotto/service/WinningNumbersService.java
+++ src/main/java/lotto/service/WinningNumbersService.java
@@ -3,14 +3,23 @@ package lotto.service;
 import java.util.List;
 import lotto.domain.WinningNumbers;
 
-public class WinningNumberService {
-
+public class WinningNumbersService {
+    /**
+     * 당첨 번호 생성
+     * @param numbers
+     * @return
+     */
     public WinningNumbers createWinningNumber(List<Integer> numbers){
         return new WinningNumbers(numbers);
     }
 
+    /**
+     * 보너스 번호 생성(당첨 번호와 중복인지 체크)
+     * @param winning
+     * @param bonusNumber
+     */
     public void ensureBonusNotDuplicated(WinningNumbers winning, int bonusNumber) {
-        if (winning.getNumbers().contains(bonusNumber)) {   // 도메인 메서드 사용 (권장)
+        if (winning.getNumbers().contains(bonusNumber)) {   // winning.contains()메서드 추가?
             throw new IllegalStateException("보너스 번호가 당첨 6개와 중복될 수 없습니다.");
         }
     }
