d0e1e21571bb88bb243e7b1fba13c1b8e4812b67
Seonggyun01 <thdrbs010314@naver.com>
2025-11-03 13:22:20 +0900

    test: Lotto 도메인 단위 테스트 추가
    - 로또 번호 개수 초과/미만 예외 테스트
    - 중복 번호 입력 시 예외 테스트
    - 0 포함 시 예외 발생 테스트
    - 45 초과 값 입력 시 예외 테스트
    - 번호 오름차순 정렬 여부 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,25 +1,60 @@
 package lotto.domain;
 
+import lotto.domain.exception.NumErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NumErrorMessage.LOTTO_NUMBER_SIZE.getMessage());
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NumErrorMessage.DUPLICATE_NUMBER.getMessage());
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호의 개수가 6개 미만이면 예외가 발생한다.")
+    @Test
+    void 로또_번호의_개수가_6개가_미만이면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NumErrorMessage.LOTTO_NUMBER_SIZE.getMessage());
+    }
+
+    @DisplayName("숫자에 0이 포함되면 예외가 발생한다.")
+    @Test
+    void 숫자에_0이_포함되면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage(NumErrorMessage.ZERO_NOT_ALLOWED.getMessage());
+    }
+
+    @DisplayName("숫자가 45를 초과하면 예외가 발생한다.")
+    @Test
+    void 숫자가_45를_초과하면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage(NumErrorMessage.OUT_OF_RANGE.getMessage());
+    }
+
+    @DisplayName("번호는 오름차순 정렬된다.")
+    @Test
+    void 번호는_오름차순으로_정렬된다() {
+        Lotto lotto = new Lotto(List.of(8, 1, 45, 3, 4, 10));
+        assertThat(lotto.getNumbers()).containsExactly(1, 3, 4, 8, 10, 45);
+    }
+
 }
