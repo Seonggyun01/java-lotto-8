42569d64dc65225f26d132d41081031917a3c310
Seonggyun01 <thdrbs010314@naver.com>
2025-11-03 12:06:18 +0900

    feat(service): LottoNumberGenerator 클래스 추가
    - 6개의 중복되지 않은 램던 숫자 추출기
    - Randoms.pickUniqueNumbersInRange()를 사용

--- src/main/java/lotto/service/LottoNumberGenerator.java
+++ src/main/java/lotto/service/LottoNumberGenerator.java
@@ -1,4 +1,18 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class LottoNumberGenerator {
+    private static final int LOTTO_NUMBER_START_RANGE = 1;
+    private static final int LOTTO_NUMBER_LAST_RANGE = 45;
+    private static final int LOTTO_SIZE = 6;
+
+    /**
+     * 로또 번호 랜덤 생성
+     * @return
+     */
+    public List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_START_RANGE, LOTTO_NUMBER_LAST_RANGE, 6);
+    }
 }
