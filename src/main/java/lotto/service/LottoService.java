f7e6f8e013942f8c10b23cbb275875958b255125
Seonggyun01 <thdrbs010314@naver.com>
2025-11-03 12:04:55 +0900

    feat(service): LottoService 클래스 추가
    - 로또 수량에 만큼 로또 생성하는 서비스
    - lottoNumberGenerator의 generate()에서 6개의 랜덤 번호를 만들어서 주입받는다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,27 @@
 package lotto.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+
 public class LottoService {
+    private final LottoNumberGenerator lottoNumberGenerator;
+
+    public LottoService(LottoNumberGenerator lottoNumberGenerator) {
+        this.lottoNumberGenerator = lottoNumberGenerator;
+    }
+
+    /**
+     * 수량만큼 로또 생성
+     * @param count
+     * @return
+     */
+    public List<Lotto> createLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = lottoNumberGenerator.generate();
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
 }
