db5176a21d07ffdf324a81e7d262e42ad0314bc3
Seonggyun01 <thdrbs010314@naver.com>
2025-11-03 12:03:43 +0900

    feat(service): PurchaseService 클래스 추가
    - 구입 금액에 따른 로또 수량을 반환하는 서비스

--- src/main/java/lotto/service/PurchaseService.java
+++ src/main/java/lotto/service/PurchaseService.java
@@ -1,4 +1,15 @@
 package lotto.service;
 
+import lotto.domain.Money;
+
 public class PurchaseService {
+    /**
+     * 금액에 맞는 로또 수량 구하기
+     * @param moneyAccount
+     * @return
+     */
+    public int run(int moneyAccount){
+        Money money = new Money(moneyAccount);
+        return money.buyLotto();
+    }
 }
